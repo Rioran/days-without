5ff8b6d224bbc652bf817ba355e2051d63171493
Rioran <33312275+Rioran@users.noreply.github.com>
2022-11-16 06:07:14 +0400

    + initial functionality

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# rioran-clock
-A simple HTML+JS clock for personal use.
+# days-without
+A simple HTML+JS page to show the amount of days without... what?
 
-Double-click the main.html file to get it running.
+As of 2022-11-16 the project is deployed on https://rioran.github.io/days-without/main
 
-As of 2022-01-26 the project is deployed on https://rioran.github.io/rioran-clock/main
+Checks for updates every 10 minutes.
+
+Wanna your custom days count? Add a URL parameter "start" as a text date in ISO format and a parameter "text" with whatever message you want. Could look like this:
+
+https://rioran.github.io/days-without/main?start=2022-11-10&text=kawabanga
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,48 @@
-var ms_render_delay = 40;
-var time_cache = new Date().toTimeString().slice(0,8);
-var clock_digits_p = document.getElementById("clock-digits-p");
+const MS_RENDER_DELAY = 1000 * 60 * 10; // every 10 minutes
+const MS_IN_A_DAY = 1000 * 60 * 60 * 24;
+const text_header_p = document.getElementById("text-header-p");
+const days_without_p = document.getElementById("days-without-p");
+const universal_day_string = "2022-11-15";
+const universal_text = "Рома продержался дней без колы:";
+const query_string = window.location.search;
+const url_params = new URLSearchParams(query_string);
 
-function render_time() {
-    var time = new Date().toTimeString().slice(0,8);
-    if (time !== time_cache) {
-      clock_digits_p.innerHTML = time;
-      time_cache = time;
+var date_cache = "current date goes here";
+var start_day;
+var text;
+
+
+function get_parameter(parameter_name, parameter_default) {
+    var parameter_value = url_params.get(parameter_name);
+    if (parameter_value == null) {
+        parameter_value = parameter_default;
+    }
+    return parameter_value;
+}
+
+function set_up() {
+    start_day = get_parameter("start", universal_day_string);
+    start_day = new Date(start_day).toISOString().split('T');
+    text = get_parameter("text", universal_text);
+    text_header_p.innerHTML = text;
+    render_days();
+}
+
+function render_days() {
+    var today = new Date().toISOString().split('T');
+    if (today !== date_cache) {
+        var date_diff = (new Date(today)) - (new Date(start_day));
+        days_without_p.innerHTML = Math.floor(date_diff / MS_IN_A_DAY);
+        date_cache = today;
     }
 }
 
 function render_loop() {
-  setInterval(function(){
-    render_time();
-  }, ms_render_delay);
+    setInterval(function(){
+        render_days();
+    }, MS_RENDER_DELAY);
 }
 
+
+set_up();
 render_loop();
